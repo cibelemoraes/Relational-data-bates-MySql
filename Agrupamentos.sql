d2fef81f2d7527c4204392df78ed5864c1583e20
Your Name <you@example.com>
2024-04-20 12:39:42 -0300

    ORDER BY AND GROUP BY

---
+++ Agrupamentos.sql
@@ -0,0 +1,50 @@
+-- CLÁUSULAS DE ORDENAÇÃO
+
+use company_constraints;
+select * from employee order by Fname, Lname;
+
+
+-- nome do departamento, nome do gerente
+
+select distinct d.Dname, concat(e.Fname, '', e.Lname) as Maneger, Adress 
+    from departament as d, employee as e, works_on as w, project p
+     where (d.Dnumber = e.Dno and e.Ssn=d.Mgr_ssn and  w.Pno = p.Pnumber)
+     order by(d.Dname, e.Fname, e.Lanme);
+     
+-- recuperando todos os empregados e seus projetos em andamento 
+select distinct  d.Dname as Department , concat(e.Fname, ' ', e.Lname) as Employee, p.Pname as Project_Name
+    from departament as d, employee e, works_on w, projetc p
+    where (d.Dnumber = e.Dno and e.Ssn = w.Essn and w.Pno = p.Pnumber)
+    order by d.Dname desc, e.Fname asc, e.Lname asc;
+    
+-- funções e cláusulas de agrupamento 
+
+select * from employee;
+
+select count(*) from employee;
+
+
+select count(*) from employee, departament;
+   where Dno-Dnumber and Dname ='Research';
+
+
+select Dno, count(*), round(avg(Salary),2) 
+   from employee, 
+   group by Dno;
+
+
+select Dno, count(*) as Number_of_employees, round(avg(Salary),2) as Salary_avg
+   from employee, 
+   group by Dno;
+
+
+select Pnumber, Pname, count(*)
+   from projetc, works_on
+   where Pnumber = Pno
+   group by Pnumber, Pname;
+
+select COUNT(distinct Salary) 
+    FROM Employee;
+    
+select sum(Salary) as total_sal, max (Salary) as total_sal, min (Salary) as Min_sal, avg(Salary) AS AVG_SAL
+    FROM Employee
